220e03181d82e1d776550b2704f25650af5d0144
singularitti <singularitti@outlook.com>
2023-03-01 19:01:52 -0500

    Add "Test subtyping from `Any`" & "Test subtyping from an abstract type"

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,16 @@ using PythonCallHelpers
 using Test
 
 @testset "PythonCallHelpers.jl" begin
-    # Write your tests here.
+    @testset "Test subtyping from `Any`" begin
+        @pymutable T Any o
+        @test supertype(T) == Any
+        @test fieldnames(T) == (:o,)
+    end
+
+    @testset "Test subtyping from an abstract type" begin
+        abstract type MyType end
+        @pyimmutable My MyType o
+        @test supertype(My) == MyType
+        @test fieldnames(My) == (:o,)
+    end
 end
